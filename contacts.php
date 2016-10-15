e7ff4e951958f9c4f2f62e7f504c7189624f46c8
Mark Tomlin <Dygear@gmail.com>
2016-10-15 05:59:01 -0400

    Fixed formatting.

--- contacts.php
+++ contacts.php
@@ -9,10 +9,10 @@ require_once('bootstrap.php');
         <title>PHP Contacts API Tutorial</title>
     </head>
     <body>
-<?php  if (!Service\OAuth::loggedIn()): ?>
+<?php   if (!Service\OAuth::loggedIn()): ?>
         <!-- User not logged in, prompt for login -->
         <p>Please <a href="<?=Service\OAuth::getLoginUrl(Service\OAuth::getRedirectUri())?>">sign in</a> with your Office 365 or Outlook.com account.</p>
-<?php  else: ?>
+<?php   else: ?>
 <?php      $contacts = Service\Outlook::getContacts($_SESSION['access_token'], $_SESSION['user_email']); ?>
         <!-- User is logged in, do something here -->
         <h2>Your contacts</h2>
@@ -22,14 +22,14 @@ require_once('bootstrap.php');
                 <th>Last Name</th>
                 <th>Email Address</th>
             </tr>
-<?php      foreach($contacts['value'] as $contact): ?>
+<?php       foreach($contacts['value'] as $contact): ?>
             <tr>
                 <td><?=$contact['GivenName']?></td>
                 <td><?=$contact['Surname']?></td>
                 <td><?=$contact['EmailAddresses'][0]['Address']?></td>
             </tr>
-<?php      endforeach; ?>
+<?php       endforeach; ?>
         </table>
-<?php  endif; ?>
+<?php   endif; ?>
     </body>
 </html>
